0b2e6be4a6b910d119cd4747d50e3758fe840f83
Danix43 <daninitu39@gmail.com>
2021-09-23 22:06:48 +0300

    Figured out commands

--- main.py
+++
@@ -1,6 +0,0 @@
-''' TODO:
-    - Add support for playing videos from youtube
-    - Add commands to the bot ("$play link", $skip, $join, $leave, $queue)
-    - Add support for a queue of songs
-    - Check the role of the user and allow to use commands based on his roles
-'''
